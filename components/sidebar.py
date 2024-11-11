a0e9da5209ee8008207aa788be92fa68f478d71a
Aadrika <159729009+paprika1212@users.noreply.github.com>
2024-11-11 22:17:44 +0000

    initial

---
+++ components/sidebar.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "2. Upload a CSV file📄 or import directly from Google Sheets\n"
+            "3. Enter a query to search💬\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
+            value=os.environ.get("OPENAI_API_KEY", None)
+            or st.session_state.get("OPENAI_API_KEY", ""),
+        )
+
+        st.session_state["OPENAI_API_KEY"] = api_key_input
+
+        st.markdown(
+            "BLA BLA"
+            
+        )
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,43 @@
 import streamlit as st
+from components.sidebar import sidebar
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Fetchify", page_icon="🔎", layout="wide")
+st.header("Fetchify 🔎")
+
+sidebar()
+
+openai_api_key = st.session_state.get("OPENAI_API_KEY")
+if not openai_api_key:
+    st.warning(
+        "Enter your OpenAI API key in the sidebar. You can get a key at"
+        " https://platform.openai.com/account/api-keys."
+    )
+
+uploaded_file = st.file_uploader("Upload a CSV file", accept_multiple_files=False)
+if uploaded_file is not None:
+    try:
+        df = pd.read_csv(uploaded_file)
+        st.write("### Full Dataset")
+        
+        selection = st.dataframe(
+                df,
+                key="interactive_df",
+                on_select="rerun",
+                use_container_width=True,
+                height=400,
+                selection_mode="single-column"
+            )
+        
+
+
+        columns = df.columns.tolist()
+        selected_column = st.selectbox("Select column to filter by", columns)
+
+        filtered_df = df[selected_column]
+        unique_values = df[selected_column].unique()
+        entries = df.iloc[:, [selected_column]]
+        st.write(filtered_df)
+
+    except:
+        st.write("error")
